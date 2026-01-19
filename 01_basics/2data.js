d12ca8e52807fad8d57ffd48220c2502e9dd97d1
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-19 12:55:53 +0000

    data types

---
+++ 01_basics/2data.js
@@ -0,0 +1,24 @@
+// Primitive(immutable)  =  Number  String  Boolean  Undefined  Null  Symbol  BigInt
+
+let a = 0.999999999999999999999999999;
+console.log(a);
+console.log(typeof a);
+
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(10/0);
+
+console.log(typeof "harsh");
+
+console.log(typeof true);
+
+let m ;
+console.log(m);
+
+let n = null
+console.log(n)
+console.log(typeof null)
+
+let num = 123456789012345678901234567890n;
+console.log(num)
+console.log(typeof num)
