572a97331cf9822e6ddb6bb0483699abc840d2e8
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-19 12:55:32 +0000

    number is internally float

---
+++ 01_basics/3number.js
@@ -0,0 +1,12 @@
+// Number is only one numeric type
+// JavaScript numbers are stored as:
+// 64-bit floating-point (IEEE 754)
+// This explains many strange behaviors.
+// Everything is stored as floating-point numbers internally
+
+console.log(0.1+0.2)    // precesion problem
+console.log(0.9999999)
+console.log(0.999999999999999999999)
+
+let num1 = 10;
+console.log(num1,typeof num1)
