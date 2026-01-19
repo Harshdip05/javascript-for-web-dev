1b6df639ebfd15a9aceb27ef1af464534d61c8c5
Harshdip05 <harshdipnandre05@gmail.com>
2026-01-19 12:56:21 +0000

    variable are container

---
+++ 01_basics/1var.js
@@ -0,0 +1,26 @@
+// JavaScript is a programming language used to make websites interactive.
+// Without JavaScript, websites would be static (only text and images).
+
+// A variable is a container used to store data.
+
+let name = "Rahul";
+// name → variable
+// "Rahul" → value stored inside variable
+
+// without variables
+console.log("Rahul");
+
+
+const PI = 3.14;
+let a = 23;
+var b = 10;
+c = 15;
+
+console.log(PI)
+console.log(a)
+console.log(b)
+console.log(c)
+
+// const =  block scoped, cannot be reassigned and redeclared
+// let = block scoped, can be resigned but cannot be redeclared
+// var = functional scope, can be reasigned and redeclared
