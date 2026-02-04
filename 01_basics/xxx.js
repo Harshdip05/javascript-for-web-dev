9b9f21e37aa2bede1da46c220bdd959e227e2050
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 05:45:02 +0000

    Learn

--- 01_basics/xxx.js
+++ 01_basics/xxx.js
@@ -2,14 +2,18 @@ let name = "harsh"
 
 let rollNo = 5
 
-console.log(name)
-console.log(rollNo)
+console.log(name,typeof name)
+console.log(rollNo,typeof rollNo)
 
 let email = "h@gmail.com"
-console.log(email)
+console.log(email,typeof email)
 
 email = "h1@gmail.com"  
-console.log(email)
+console.log(email,typeof email)
 
 // let email = "h3@gmail.com"
-console.log(email)
+console.log(email,typeof email)
+
+let emailPassword;
+console.log(emailPassword)
+console.log(typeof emailPassword)
