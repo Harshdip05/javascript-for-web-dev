7f7ac7ccf7be5fc10682a38a50a593854159de3c
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 11:33:28 +0000

    Que practice

---
+++ 02_basics/que.js
@@ -0,0 +1,21 @@
+stri = "abcd"
+
+if (stri[0]==="a" && stri.length>3){
+    console.log("string is good")
+}
+else{
+    console.log("string is not good")
+}
+
+
+// 
+
+
+let num = 12;
+
+if ((num%3===0) && ((num+1==15) || (num-1==11))){
+    console.log("safe")
+}
+else{
+    console.log("not safe")
+}
