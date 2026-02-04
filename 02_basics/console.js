edf79de77938a333619ed692bb302941d48b3a6f
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 09:10:08 +0000

    Learn console

---
+++ 02_basics/console.js
@@ -0,0 +1,25 @@
+// console.log() is a JavaScript function used to print information to the console for debugging and understanding how your code runs.
+// console.log() in the Browser vs Node.js
+
+console.log("hello world")
+
+let a = 5
+let b = 7
+console.log(a, b, a + b)
+
+console.log(console.log("Hi"))
+
+
+
+console.error("Something went wrong")
+
+
+
+const users = [
+  { name: "A", age: 20 },
+  { name: "B", age: 25 }
+]
+console.table(users)    // visualizing structured data
+
+
+console.log()
