c01cc06f1cba2e514c49d9dbcd1aa40e97b04a57
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 12:02:11 +0000

    Learn prompt() in js

---
+++ 02_basics/prompt.js
@@ -0,0 +1,4 @@
+// prompt() is a built-in browser function that shows a modal dialog asking the user for input.
+// It is part of the browser (window) API, not core JavaScript
+
+const name = prompt("What is your name?");
