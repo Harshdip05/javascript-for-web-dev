aa792fb1a873ccf2fa865e715e342feeaa99a93b
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 11:33:08 +0000

    Que of if else

---
+++ 02_basics/ifelseque.js
@@ -0,0 +1,33 @@
+let trafficLight =  "red"
+
+if (trafficLight=="red"){
+    console.log("stop")
+}
+if (trafficLight=="green"){
+    console.log("go")
+}
+if (trafficLight=="yellow"){
+    console.log("wait")
+}
+
+
+// 
+
+
+let popcornSize = "Xl"
+
+if (popcornSize==="XL"){
+    console.log("price is 250")
+}
+else if (popcornSize==="L"){
+    console.log("price is 200")
+}
+else if(popcornSize==="M") {
+    console.log("price is 150")
+}
+else if (popcornSize==="S"){
+    console.log("price is 100")
+}
+else{
+    console.log("you entered kuch alag")
+}
