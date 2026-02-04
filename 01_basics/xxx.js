a519714941b79cca4a0d4484927a0aa5361de6b8
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-04 05:38:19 +0000

    Learn let keyword in js

---
+++ 01_basics/xxx.js
@@ -0,0 +1,15 @@
+let name = "harsh"
+
+let rollNo = 5
+
+console.log(name)
+console.log(rollNo)
+
+let email = "h@gmail.com"
+console.log(email)
+
+email = "h1@gmail.com"  
+console.log(email)
+
+// let email = "h3@gmail.com"
+console.log(email)
