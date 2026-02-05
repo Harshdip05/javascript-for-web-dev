d55f6ea5000acf7cb64db87cadcdeb2fe98ef080
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-05 17:50:00 +0000

    Questions of js

---
+++ 02_basics/jsque.js
@@ -0,0 +1,20 @@
+let num = 10;
+
+if (num%10===0){
+    console.log("good")
+}
+else{
+    console.log("bad")
+}
+
+// 
+
+
+let name = prompt("write name: ")
+let age = prompt("write age: ")
+
+console.alert(`your name is ${name} and age is ${{age}}`)
+
+
+// 
+
