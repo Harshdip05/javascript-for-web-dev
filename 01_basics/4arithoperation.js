cfa85269c1fbbf8de0a9231354a07abcd9de1f19
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 08:21:00 +0000

    Learn arithmetic operator

--- 01_basics/4arithoperation.js
+++ 01_basics/4arithoperation.js
@@ -1,2 +1,19 @@
-// operand operator
+// operand, operator, operation
+        
+        //       ()
+        //       **
+        //       * / %
+        //       + -
 
+
+
+let a = 5;;
+let b = 10;
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(Math.floor(a/b))
+console.log(a**b)
