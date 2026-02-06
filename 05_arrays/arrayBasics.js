d8b3957111d3905f280dddcab0fa5f8be9ab6f44
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 12:20:20 +0000

    Basics of array in js

---
+++ 05_arrays/arrayBasics.js
@@ -0,0 +1,28 @@
+// An array is an ordered collection of elements stored in contiguous memory (conceptually) and accessed using indexing (0-based).
+
+let student1 =  "Harsh"
+let student2 = "Raj"
+let student3 = "Varun"
+
+let arrayStudent = ["Harsh","Raj","Varun"]
+console.log(arrayStudent)
+
+console.log(arrayStudent.length)
+console.log(arrayStudent[0])
+console.log(arrayStudent[1])
+console.log(arrayStudent[2])
+console.log(arrayStudent[3])
+
+console.log(arrayStudent[0][0])
+console.log(arrayStudent[0].length)
+
+// 
+
+let nums = [2,4,6,8]
+console.log(nums)
+console.log(typeof nums)
+
+
+let mixedArray = [1,"Harsh",true]
+
+let emptyArray = []
