16633ddb9f9dc4dd7de37cef4b2f3101488efad7
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 16:34:58 +0000

    Learn indeOf() , concat() , reverse()

---
+++ 05_arrays/arrayMethods2.js
@@ -0,0 +1,22 @@
+let primary  = ["red","yellow","blue"];
+
+console.log(primary.indexOf("red"));
+console.log(primary.indexOf("Yello"));
+console.log(primary.indexOf("green"));
+
+// 
+
+console.log(primary.includes("red"));
+console.log(primary.includes("green"));
+
+// concatennate in strings check
+
+let secondary = ["orange","green","violet"];
+
+console.log(primary.concat(secondary));
+console.log(secondary.concat(primary));
+
+// 
+
+console.log(primary.reverse());
+console.log(primary);  // changes original array
