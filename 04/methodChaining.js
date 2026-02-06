9f125f368d46a129b6e274b8d7e04e2c30fd9a75
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 10:07:25 +0000

    Learn one method after other

---
+++ 04/methodChaining.js
@@ -0,0 +1,11 @@
+// Method chaining means calling multiple methods one after another in a single line.
+// Each method returns something → the next method works on that result.
+
+let msg = "    hello   "
+console.log(msg)
+let msgTrim = msg.trim()
+console.log(msgTrim)
+let msgTrimUpper = msgTrim.toUpperCase()
+console.log(msgTrimUpper)
+
+console.log(msg.trim().toUpperCase())
