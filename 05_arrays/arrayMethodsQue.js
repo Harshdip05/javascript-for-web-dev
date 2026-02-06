cb164d2c45ecb7b0fb92895c664b602df6e5ea13
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 13:25:47 +0000

    Que on shift and unshift

---
+++ 05_arrays/arrayMethodsQue.js
@@ -0,0 +1,15 @@
+let start = ["Jan","July","March","Aug"]
+
+console.log(start,start.length)
+
+console.log(start.shift("Jan"),start.length)
+
+console.log(start)
+
+console.log(start.shift())
+console.log(start)
+
+console.log(start.unshift("June"))
+console.log(start.unshift("July"))
+
+console.log(start)
