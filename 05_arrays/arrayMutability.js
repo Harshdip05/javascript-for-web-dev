d2dcf82ea2b8d4585cbc52fb8986cfb11dcdefb5
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 12:44:32 +0000

    Mutability in arrays

---
+++ 05_arrays/arrayMutability.js
@@ -0,0 +1,14 @@
+let fruits = ["Mango","Banana","Apple"]
+console.log(fruits)
+console.log(fruits.length)
+
+fruits[0] = "Watermelon"
+
+console.log(fruits)
+console.log(fruits.length)
+
+
+fruits[10] = "Dragonfruit"
+
+console.log(fruits)
+console.log(fruits.length)
