86886988c41f0954a9fb397868eacf13703fdd7d
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 13:03:21 +0000

    Learn push pop shift and unshift

---
+++ 05_arrays/arrayMethods.js
@@ -0,0 +1,15 @@
+let cars = ["bmw","audi","tata"]
+
+console.log(cars,cars.length)
+
+console.log(cars.push("toyota"),cars.length);
+
+console.log(cars.push("kia"),cars.length)
+
+console.log(cars.pop(),cars.length)
+
+console.log(cars.unshift("mahindra"),cars.length)
+
+console.log(cars.shift(),cars.length)
+
+console.log(cars,cars.length)
