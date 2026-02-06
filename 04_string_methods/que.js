bd7bf7b4a84215b4b6c7b7b801dec4e12af829cd
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 10:39:43 +0000

    Questions of string methods

---
+++ 04_string_methods/que.js
@@ -0,0 +1,15 @@
+let msg = "  help!    "
+console.log(msg.trim().toUpperCase())
+
+
+let name = "ApnaCollege"
+console.log(name.slice(4,9))
+console.log(name.indexOf("na"))
+console.log(name.replace("Apna","Our"))
+
+
+
+console.log(name.slice(4).replace("l","t"))
+console.log(name.substring(4).replace("l","t"))
+console.log(name.slice(4).replaceAll("l","t"))
+console.log(name.substring(4).replaceAll("l","t"))
