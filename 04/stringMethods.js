ab8691ef754a0b908fc35b5d31e4fc6e58c8a860
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 09:21:47 +0000

    Trim whitespace from string

---
+++ 04/stringMethods.js
@@ -0,0 +1,35 @@
+// Strings in JS are immutable → methods return new strings.
+
+let name = "Javascript is good language.";
+
+console.log(name.length);
+
+console.log(name.toLowerCase);
+console.log(name.toUpperCase());
+
+console.log(name.trim());
+
+console.log(name.includes("is"));
+
+console.log(name.startsWith("Java"));
+console.log(name.endsWith("language"));
+
+
+console.log(name.split(" "));
+
+console.log(name.split(" ").join(" "))
+
+
+console.log("hi hi".replace("hi", "hello")); 
+console.log("hi hi".replaceAll("hi", "hello"));
+
+// indexOf() / lastIndexOf()    = returnn -1 if not found
+console.log("banana".indexOf("a"));     
+console.log("banana".lastIndexOf("a"));
+
+
+console.log("JavaScript".slice(0, 4));
+
+console.log("JavaScript".substring(4, 10));  // no negative index
+
+
