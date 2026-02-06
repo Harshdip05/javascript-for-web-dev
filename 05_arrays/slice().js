d340b35be9bfda09081a40bf5e54abc7ea479e32
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-06 16:27:43 +0000

    Slice in arrays

---
+++ 05_arrays/slice().js
@@ -0,0 +1,15 @@
+// slice 
+
+let  colors = [ 'orange', 'green', 'violet', 'red', 'yellow', 'blue' ];
+
+console.log(colors.slice())
+
+console.log(colors.slice(1));
+console.log(colors.slice(1,colors.length));
+
+console.log(colors.slice(2,3));
+
+console.log(colors.slice(-2));
+console.log(colors.slice(-2,colors.length));
+
+console.log(colors.slice(colors.length))
