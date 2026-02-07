7f53f5513beac3a400daf65e388dd4d23483cbdf
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 10:47:10 +0000

    Multiplication  table using loop

---
+++ 06-loops/multiplicationTable.js
@@ -0,0 +1,26 @@
+
+for(let i = 1;i<=10;i++){
+    console.log(5,"X",i,"=",5*i)
+}
+
+
+for(let i = 5;i<=50;i=i+5){
+    console.log(i)
+}
+
+
+for(let i = 4;i<=40;i=i+4){
+    console.log(i)
+}
+
+
+// 
+
+
+let userNum = prompt("write your num");
+userNum = (parseInt(userNum))
+// let userNum = Number(prompt("write your num"));
+
+for(let i = userNum; i<=userNum*10;i=i+userNum){
+    console.log(i)
+}
