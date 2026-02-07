de5e0315ab271e31bba3e0fe0af6cf3378864b2e
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 10:36:24 +0000

    Learn infinite loops

---
+++ 06-loops/infiniteLoops.js
@@ -0,0 +1,21 @@
+// An infinite loop is a loop that never stops running because its exit condition is never met.
+// The condition is always true or the loop variable never changes in the right direction.
+
+for(let i = 1;i>=0;i++){
+
+}
+
+for(let i = 1;i<=5;i--){
+
+}
+
+for(let i =1 ; ; i++){
+//  If the condition is missing, JavaScript treats it as true.   
+}
+
+
+
+// CPU keeps running → browser tab may freeze
+// Blocks the JavaScript thread
+// Freezes browser / Node.js process
+// Needs force stop (Ctrl + C or close tab)
