3edf0d97b0fd9b1bb23c4773bdd61a6f59dec53d
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 10:04:47 +0000

    Implement for loop logic

---
+++ 06-loops/forLoops.js
@@ -0,0 +1,38 @@
+//         for (initialization; condition; increment/decrement) {
+//                // code to run
+//           }
+
+
+for (let i = 1; i<=5;i++){
+    console.log(i)
+}
+
+for (let i = 1; i<=5;i = i+1){
+    console.log(i)
+}
+
+for (let i = 1; i<=5;++i){
+    console.log(i)
+}
+
+
+console.log()
+
+
+let x = 5;
+console.log(x)
+console.log(x++); // 5 (then x becomes 6)
+console.log(x)
+console.log(++x); // 7 (x becomes 7 first)
+console.log(x)
+
+
+let m = 2
+console.log(m);
+m++;
+console.log(m)
+
+let n = 4
+console.log(n);
+++n;
+console.log(n)
