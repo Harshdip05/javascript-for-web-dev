c7abaf34b5b77df3400eea33c20d0fa73d3b7128
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 12:09:00 +0000

    Learn for of loop

---
+++ 06-loops/forOfLoop.js
@@ -0,0 +1,14 @@
+// for...of is used to iterate over values of an iterable (like arrays, strings, maps, sets).
+// It gives you values directly, not indexes.
+
+let fruits = ["mmango","apple","banana","litchi","orange"]
+
+for(fruit of fruits){
+    console.log(fruit);
+}
+
+// 
+
+for(char of "apnacollege"){
+    console.log(char)
+}
