ebc0f045543eafc281f77951bbdddef6939bcd09
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 11:30:02 +0000

    Guess favourite movie

--- 06-loops/favouriteMovie.js
+++ 06-loops/favouriteMovie.js
@@ -5,3 +5,9 @@ while(guess!==favourite  && guess!=="quit"){
     console.log("wrong");
     guess = prompt("guess again");
 }
+
+if (guess === favourite) {
+  console.log("Correct! You guessed the movie ");
+} else {
+  console.log("You quit the game");
+}
