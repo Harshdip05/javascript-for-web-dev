ad36ea604e00568afd99ab212797d4f811605536
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-07 11:10:50 +0000

    While loop

---
+++ 06-loops/whileLoops.js
@@ -0,0 +1,23 @@
+// A while loop repeats a block of code as long as a condition is true.
+// The condition is checked before the loop runs each time.
+
+let i = 1;
+while (i<=5){
+    console.log(i);
+    i++;
+}
+
+
+let j = 5;
+while(j>=1){
+    console.log(j);
+    j--;
+}
+
+
+
+let n = 0;
+while(n<=20){
+    console.log(n);
+    n=n+2;
+}
