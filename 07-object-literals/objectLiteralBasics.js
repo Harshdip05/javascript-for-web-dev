bced2f87d22ac049eaaa0f4ab57d8de627a976fa
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-12 02:26:37 +0000

    Learn basics of object literal

---
+++ 07-object-literals/objectLiteralBasics.js
@@ -0,0 +1,23 @@
+// Keys (also called properties) are strings (or Symbols)
+// Values can be anything: primitives, arrays, functions, other objects
+// 👉 Objects let you group related data and behavior.
+
+const user = {
+  name: "Alex",
+  age: 25,
+  isAdmin: true
+};
+
+// user is collection of 3 properties(key-value pair) = object-literal
+console.log(typeof user);
+console.log(user);
+
+
+// 
+
+let arr = [];
+console.log(typeof arr)  // object
+
+
+
+// object-literal VS object
