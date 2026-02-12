218961b26b14f0aa09076ae674cb0dd8baf057f5
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-12 02:36:58 +0000

    Create object literals

---
+++ 07-object-literals/creatingObjectLiterals.js
@@ -0,0 +1,22 @@
+let student1 = {
+    name:"harsh",
+    rollNo:5,
+    marks:94
+}
+console.log(typeof student1,student1)
+
+let student2 = ["harsh",5,94]
+console.log(typeof student2,student2)
+
+
+// more clarity in object literals than in array
+
+// object order is diff and in array order is defined
+
+
+const item = {
+    price:100,
+    discount:10,
+    colors:["red","yellow","blue"]
+}
+console.log(typeof item,item)
