6fa96ffa36a65408306023a191a0ddd071bf7267
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-12 02:42:04 +0000

    Create post of twitter

---
+++ 07-object-literals/creatingPost.js
@@ -0,0 +1,12 @@
+// twitter/thread post
+// Object literals model real-world entities by grouping related data and relationships.
+
+const post ={
+    username:"Harsh05",
+    content:"This is my #1 post",
+    likes:150,
+    repost:10,
+    tags:["delta","omega"]
+}
+
+console.log(post)
