b9e0ac13cf1fbf992a75d86dc3552dc74e670cf8
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 06:21:11 +0000

    Learn identifier rules

---
+++ 01_basics/10identifier.js
@@ -0,0 +1,36 @@
+let harsh ;
+
+let price123;
+
+let priceNew;
+
+// let price-; 
+
+// let old price;
+
+let oldPrice;
+
+let $price;
+
+let _price;
+
+// let 1price;
+
+let price;
+let Price;
+
+// let var;
+
+
+
+
+// ***********************************
+
+
+const MAX_LIMIT = 100;    // UPPER_CASE
+
+let priceOld;     // camelCase
+
+// class UserAccount{}     // PascalCase
+
+
