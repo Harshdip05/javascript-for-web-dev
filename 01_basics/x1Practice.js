bb3a62966ee70fc6e72e64a32d50e4729bf8c820
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 05:50:30 +0000

    Practice que

---
+++ 01_basics/x1Practice.js
@@ -0,0 +1,14 @@
+let age = 5;
+console.log(age);
+age + 2;
+console.log(age)
+age = age +2;
+console.log(age)
+
+
+
+let m1 = 80;
+let m2 = 90;
+let m3 = 95;
+let avg = (m1+m2+m3)/3;
+console.log(avg);
