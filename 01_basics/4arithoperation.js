08bd0bac781b258853b10cb11962eb1a86c9d4e8
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 05:30:37 +0000

    Learn arithmetic operator

--- 01_basics/4arithoperation.js
+++ 01_basics/4arithoperation.js
@@ -14,6 +14,15 @@ console.log(a+b)
 console.log(a-b)
 console.log(a*b)
 console.log(a/b)
-console.log(a%b)
-console.log(Math.floor(a/b))
-console.log(a**b)
+console.log(a%b)               // modulo operator 
+console.log(Math.floor(a/b))   // 0.5 rounds down to 0
+console.log(Math.floor(0.5)==0)
+console.log(Math.floor(0.5)===0)
+console.log(a**b)              // exponentiation(power)
+
+
+console.log(a%2==0)
+console.log(a%2==0)
+
+console.log(b%2==0)      // even number
+console.log(b%2===0)
