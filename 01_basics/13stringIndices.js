7b81c5af6f1571fa9f95917b2603fd547d10867a
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 06:31:19 +0000

    Learn string indices

---
+++ 01_basics/13stringIndices.js
@@ -0,0 +1,15 @@
+let name = "Tony Stark";
+console.log(name,typeof(name));
+
+console.log(name[0]);
+console.log(name[18]);
+
+console.log(name[-1]);
+console.log(name[-2312]);
+
+console.log(name.length);
+console.log(typeof(name.length));
+
+
+console.log("tony"+"stark");
+
