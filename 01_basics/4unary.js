d2c72aaac7df260f56831f315f5c3b2a6a449dd0
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 06:04:21 +0000

    Learn unary operator

--- 01_basics/4unary.js
+++ 01_basics/4unary.js
@@ -1,10 +1,39 @@
+// unary has only ONE operand (value).
+// binary has 2 operand
+
 let a = 1;
 console.log(a);
 
-++a;
+++a;                         // change then use
 console.log(a)
 
-a++;
+a++;                         // use then change
 console.log(a)
 
 console.log(a)
+
+
+// 
+
+let b = 5;
+console.log(b);
+
+--b;
+console.log(b);
+
+b--;
+console.log(b);
+
+
+
+
+// 
+
+let num = 5;
+console.log("num: ",num);
+let newNum = num++;
+console.log("num: ",num);
+console.log("newNum: ",newNum);
+newNum = ++num;
+console.log("num: ",num);
+console.log("newNum: ",newNum);
