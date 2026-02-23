cbe1f3ae82f017ad38f38c46d6fb0902c3ec0389
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 05:39:31 +0000

    precedence and associativity

--- 01_basics/6precedence.js
+++ 01_basics/6precedence.js
@@ -1,2 +1,14 @@
 // Operator precedence decides which operator runs first when multiple operators appear in one expression.
 // When precedence is same → associativity decides direction.
+
+
+/*
+
+
+()
+**
+* / %
++ -
+
+
+*/
