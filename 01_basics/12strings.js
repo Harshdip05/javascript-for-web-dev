cdec23295a43f2ff09e6c86e7ed9342e49685408
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 06:26:59 +0000

    Learn string data type

---
+++ 01_basics/12strings.js
@@ -0,0 +1,19 @@
+let name = "Tony Stark";
+console.log(name,typeof(name));
+
+
+let numChar = "123";
+console.log(char,typeof(char));
+
+
+let emptyString = "";
+console.log(emptyString,typeof(emptyString));
+
+
+let string1 = "fsdfsfs sdfsdfsdf%#$%#% 3234        sddfdsf";
+console.log(string1);
+
+let quote = "this is 'apple'";
+console.log(quote);
+quote = 'this is "apple"';
+console.log(quote);
