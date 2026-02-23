a6b209f805c7458ee59f511406a29d74ff85550a
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-23 05:37:48 +0000

    Learn NaN in js

--- 01_basics/5NaN.js
+++ 01_basics/5NaN.js
@@ -1,8 +1,10 @@
 // NaN = Not a Number , invalid or undefined numeric result.
 
 console.log(typeof NaN)
+console.log(typeof NaN == "number")
 console.log(typeof NaN === "number")
 // Because NaN is part of the IEEE-754 numeric standard.
+// IEEE-754 defines NaN as a numeric value, not a separate data type.
 
 console.log(0/0)
 
@@ -17,11 +19,17 @@ console.log(undefined+1)
 console.log(Math.sqrt(-1))  // NaN
 console.log(Number("abc"))
 
+
 // Any operation with NaN results in NaN.
+
 
 console.log(NaN==NaN)
 console.log(NaN===NaN)
 
+
 // isNaN() vs Number.isNaN()
+// isNaN() does type coercion, Number.isNaN() does NOT.
+// isNaN() first converts the value to a number, then checks if it is NaN.
+// Number.isNaN(): Does NOT convert Returns true only if the value is already NaN
 console.log(isNaN("abc"))
 console.log(Number.isNaN("abc"))
