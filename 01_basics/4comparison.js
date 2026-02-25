071c00e7ce2e79e43a54ea5153b9fb2c9a2c7f84
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-25 08:28:37 +0000

    comparsion

---
+++ 01_basics/4comparison.js
@@ -0,0 +1,10 @@
+let age = 18
+console.log(age>18)
+console.log(age<18)
+console.log(age<=18)
+console.log(age>=18)
+
+// loose equlity
+// strict equality
+// loose inequality
+// strict inequality
