5c8b2dd3235138f8f84baf3f964804b6f0191caa
Harshdip Nandre <harshdipnandre05@gmail.com>
2026-02-26 06:56:59 +0000

    Practice basic question on function

---
+++ 08-function/2que.js
@@ -0,0 +1,11 @@
+// function to write a poem
+
+function poem(){
+    console.log(`
+Twinkle, twinkle, little star,
+How I wonder what you are.
+Up above the world so high,
+Like a diamond in the sky.`)
+};
+
+poem();
---
+++ 08-function/3que.js
@@ -0,0 +1,8 @@
+// function to roll a dice and always  display the value of the dice(1 to 6)
+
+function rollDice(){
+    let rand = Math.floor(Math.random()*6)+1;
+    console.log(rand);
+};
+
+rollDice();
